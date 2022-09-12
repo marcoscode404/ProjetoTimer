5de2e5fce8d648f6bfcb90e4398555f10859bbd8
marcoscode404 <viniciusmg99@outlook.com>
2022-09-12 14:48:35 -0400

    iniciando projeto

---
+++ Readm.md
@@ -0,0 +1,5 @@
+<!-- install dependencias -->
+- npm i styled-components 
+- npm i @types/styled-components -D
+
+
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { ThemeProvider } from "styled-components";
+import { ButtonContainer } from "./components/Button.styles";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
+// import { useState } from 'react'
+export function App() {
+  return (
+      <ThemeProvider theme={defaultTheme}>
+        <ButtonContainer variant="primary"/>
+
+
+        <GlobalStyle />
+      </ThemeProvider>
+  )
+}
+
