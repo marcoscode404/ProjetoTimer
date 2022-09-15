8c3eaea82fe488c869f647ff951070f0fafa0e75
marcoscode404 <viniciusmg99@outlook.com>
2022-09-15 15:22:09 -0400

    estilizando os inputs

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -28,6 +28,36 @@ export const FormContainer = styled.main`
   flex-wrap: wrap;
 `
 
+// css base para os inputs
+const BaseInput = styled.input`
+  background: transparent;
+  height: 2.5rem;
+  border: 0;
+  border-bottom: 2px solid ${(props) => props.theme['gray-500']};
+  font-weight: bold;
+  font-size: 1.125rem;
+  padding: 0 0.5rem;
+  color: ${(props) => props.theme['gray-100']};
+
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme['green-500']};
+  }
+
+  &::placeholder {
+    color: ${(props) => props.theme['gray-500']};
+  }
+`
+
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+`
+
+export const MinutesAmountInput = styled(BaseInput)`
+  width: 4rem;
+`
+// \\
+
 export const CountdownContainer = styled.main`
   font-family: 'Roboto Mono, monospace';
   font-size: 10rem;
@@ -52,4 +82,32 @@ export const Separator = styled.div`
   overflow: hidden;
   display: flex;
   justify-content: center;
+`
+
+export const StartCountdownButton = styled.button`
+  width: 100%;
+  border: none;
+  padding: 1rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  gap: 0.5rem;
+  font-weight: bold;
+
+  cursor: pointer;
+
+  background: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme['gray-100']};
+
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+
+  /* se o nada tiver rodando desabilite o hover */
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['green-700']};
+  }
 `
